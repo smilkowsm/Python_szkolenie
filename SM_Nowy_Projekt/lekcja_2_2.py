d71d4293f6554650b8920c521eb27e7186099c64
smilkowsm <smilkowsm@gmail.com>
2025-09-18 20:55:44 +0200

    Dodajemy pierwsze lekcje z Python'a

---
+++ SM_Nowy_Projekt/lekcja_2_2.py
@@ -0,0 +1,11 @@
+liczba_1 = 12
+liczba_2 = 25
+
+print(liczba_1 + liczba_2)
+print(liczba_1 - liczba_2)
+print(liczba_1 * liczba_2)
+print(liczba_1 / liczba_2)
+print(liczba_1 % liczba_2) # modulo - reszta z dzielenia
+print(liczba_1 ** liczba_2) # potega
+print(liczba_1 ** (1/2)) # pierwiastkowanie
+print(liczba_1 // liczba_2) # dzielenie bez reszty
---
+++ SM_Nowy_Projekt/lekcja_2_3.py
@@ -0,0 +1,22 @@
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# CTRL + ? - komentarz linijki
+
+print(not False)
+print(not True)
+
+print(2 < 3)
+print(2 > 3)
+print(2 == 3)
+print(2 != 3)
+
+# Stringi tez mozna porownywac
+print("2" < "3")
